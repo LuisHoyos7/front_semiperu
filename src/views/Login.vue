67e6d10008ed7276a6fbdb44f41f1a31511cfbfe
Jose Tenorio <jose.ds.160899@gmail.com>
2022-10-10 11:59:56 -0500

    ultimos cambios - jose

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <button @click="me">Obtener usuario</button>
+    <pre>{{ user }}</pre>
     <form @submit.prevent="login">
       <input v-model="form.email" type="email" placeholder="email..."/><br />
       <input v-model="form.password" type="password" placeholder="password..."/><br />
@@ -13,7 +15,6 @@
 import axios from "axios";
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.min.css";
-
 axios.defaults.withCredentials = true;
 
 export default {
@@ -28,11 +29,20 @@ export default {
   }),
   
   methods:{
-
+    me(){
+       axios.get('http://localhost:8000/api/user')
+       .then(res=>{
+          console.log(res.data);
+       }).catch(err=>{
+          console.log(err.response.data);
+       });
+    },
     login(){
         axios.get('http://localhost:8000/sanctum/csrf-cookie').then(() => {
-            axios.post("http://localhost:8000/api/login", this.form).then(res =>{
-                if(res){
+            axios.post("http://localhost:8000/login", this.form).then(res =>{
+              console.log(res); 
+              if(res){
+                    this.user = res.data;
                     new Swal({
                       title : 'Exito',
                       text : 'Se realizo el inicio de sesión con exito',
